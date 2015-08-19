9b64bccdcc85ba2bd17e12a92fc0458f89e8c8f1
mcardacci <marc.cardacci@gmail.com>
2015-08-19 16:05:12 -0400

    adds edit and create action with prod params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,20 @@
 class ProductsController < ApplicationController
-	def index
-		@products = Product.all
-	end
+  def index
+    @products = Product.all
+  end
 
-	def edit
-		
-	end
+  def edit
+    @product = Product.new
+    @current_product = Product.find_by(id: params["id"])
+  end
+
+  def create
+    this is a tab test
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name)
+  end
 end
