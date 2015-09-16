13648d75cd34d5a4b8c14a3c7b666b1ed8c51b62
Marco Cardacci <marco.cardacci@gmail.com>
2015-09-16 09:51:00 -0400

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-	def self.search(search)
-		where("lower(name) LIKE ?", "%#{search.downcase}%") | where("lower(vineyard) like ?", "%#{search.downcase}%") | where("lower(description) like ?", "%#{search.downcase}%")
-	end
+  def self.search(search)
+    where("lower(name) LIKE ?", "%#{search.downcase}%") | where("lower(vineyard) like ?", "%#{search.downcase}%") | where("lower(description) like ?", "%#{search.downcase}%")
+  end
 end
